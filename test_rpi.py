8d5a8b76bf6b90b47a1ae6c98524b53b1655bc9a
Enny1991 <enea.ceolini@gmail.com>
2019-03-22 12:41:30 +0000

    mods to test_rpi

--- test_rpi.py
+++ test_rpi.py
@@ -1,12 +1,13 @@
 import numpy as np
 import time
-from model_stft import MudNoFFT
+from model_stft import Mudv3noFFT
 import json
 import os
 import torch
 from torch import nn
 from torch.autograd import Variable
 import csv
+from tqdm import tqdm
 
 
 def load_mask_model(load, base_dir='./'):
@@ -15,7 +16,7 @@ def load_mask_model(load, base_dir='./'):
         p = json.load(fff)
         load_path = json_dir + '/net/' + 'cv/'
 
-        model = MudNoFFT(n_fft=p['nfft'], kernel=(p['kernel1'], p['kernel2']), causal=p['causal'],
+        model = Mudv3noFFT(n_fft=p['nfft'], kernel=(p['kernel1'], p['kernel2']), causal=p['causal'],
                                 layers=p['layers'], stacks=p['stacks'], verbose=False)
 
         mdl_idx = sorted([int(l.split('_')[-1].split('.')[0]) for l in os.listdir(load_path)])[-1]
@@ -24,14 +25,21 @@ def load_mask_model(load, base_dir='./'):
         return model, p
 
 
-mdl, _ = load_mask_model('201903104652_baseline2_C')
 
-re_m = torch.from_numpy(np.random.randn(1, 2, 257, 128).astype('float32'))
-im_m = torch.from_numpy(np.random.randn(1, 2, 257, 128).astype('float32'))
+mdl, p = load_mask_model('201903161143_512_4l_NC')
+print(p)
+re_m = torch.from_numpy(np.random.randn(1, 2, 129, 128).astype('float32'))
+im_m = torch.from_numpy(np.random.randn(1, 2, 129, 128).astype('float32'))
 
 mix = torch.cat([re_m, im_m], -1)
 mix = Variable(mix).contiguous()
-start_time = time.time()
-recon = mdl(mix)
-stop_time = time.time() - start_time()
-print("Took {} s to process 4s of 4 mics".format(stop_time))
+
+curr = []
+
+for i in tqdm(range(20)):
+    start_time = time.time()
+    recon = mdl(mix)
+    curr.append(time.time() - start_time)
+
+
+print("Took {} +/- {} s to process".format(np.mean(curr), np.std(curr)))
